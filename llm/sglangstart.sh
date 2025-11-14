abab22eb3158453711cc8ee4b840ed954a9feba0
ramumanavalan <ramums21@gmail.com>
2025-11-14 15:54:13 +0530

    Update sglangstart.sh

--- llm/sglangstart.sh
+++ llm/sglangstart.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-rm -rf /workspace
-git clone https://${GITHUB_TKN}@github.com/axploit/sglang-qwen3.git /workspace
-cd /workspace
+rm -rf /workspace/exmrsglang
+git clone https://${GITHUB_TKN}@github.com/axploit/sglang-qwen3.git /workspace/exmrsglang
+cd /workspace/exmrsglang
 git remote set-url origin https://github.com/axploit/sglang-qwen3.git
 
 # Activate conda environment
