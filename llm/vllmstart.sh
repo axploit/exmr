ea22918e93c9d835ee6d4fb94511e46bdcc5f333
ramumanavalan <ramums21@gmail.com>
2025-12-10 16:42:09 +0530

    Update vllmstart.sh

--- llm/vllmstart.sh
+++ llm/vllmstart.sh
@@ -6,4 +6,4 @@ git clone https://${GITHUB_TKN}@github.com/axploit/vllm-qwen3.git /workspace/exm
 cd /workspace/exmrvllm
 git remote set-url origin https://github.com/axploit/vllm-qwen3.git
 
-bash start_server.sh
+# bash start_server.sh
