43739713b68759a234f891628b1705b0d885ef0e
ramumanavalan <ramums21@gmail.com>
2026-03-14 20:17:17 +0530

    Log server output to server.log
    Redirect server output to server.log

--- comfyui/start.sh
+++ comfyui/start.sh
@@ -9,4 +9,4 @@ echo "Startup preparation completed"
 # wait $DOWNLOAD_PID
 # echo "Model download completed"
 
-python3 main.py --listen 0.0.0.0 --port 8000
+python3 main.py --listen 0.0.0.0 --port 8000 >> server.log 2>&1
