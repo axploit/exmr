accbcc33916d82fafefd1c78248065e59ac4cf2c
ramumanavalan <ramums21@gmail.com>
2026-03-14 20:07:36 +0530

    Add startup script for ComfyUI

---
+++ comfyui/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+cd /workspace/comfyui/
+
+echo "Startup preparation completed"
+
+# Wait for Model to download before starting server
+# wait $DOWNLOAD_PID
+# echo "Model download completed"
+
+python3 main.py --listen 0.0.0.0 --port 8000
